16db7306742c7f9b5cbfd03852765f7449e6595b
prathamesh <prathmesh6464@gmail.com>
2020-02-29 18:47:17 +0530

    MODIFIED : Added board feature.

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,6 +2,18 @@ public class TicTacToe
 {
 	public static void main(String[] args)
 	{ 
-		System.out.println("Welcome In TictacToe");
+		int[][] board = new int[3][3];
+		for(int iter=0; iter<3; iter++)
+		{
+			for(int iter2=0; iter2<3; iter2++)
+			{	
+				if(iter2 != 0)
+				{	
+					System.out.print("|");
+				}
+				System.out.print(board[iter][iter2]);
+			}
+			System.out.println();
+		}
 	}
 }
